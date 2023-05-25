4fc332fab7fa59bf965e2e9a7603262617c3b340
WhereToPress <stolyarov.mak@gmail.com>
2023-05-25 13:45:55 +0300

    less 12. Change App.js to components Header, Navbar, Profile.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://e7.pngegg.com/pngimages/775/182/png-clipart-logo-game-maker-computer-icons-gamemaker-studio-maker-miscellaneous-game.png"
+        alt="header_img"
+      ></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg"
+          alt="content-main-img"
+        ></img>
+      </div>
+      <div>
+        <img
+          className="content__ava_img"
+          src="https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-531.jpg"
+        ></img>
+        ava + descriprion
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+          <div>post 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
