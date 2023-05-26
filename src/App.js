4fa7d6fd6152cd40ff547b819a2946ec2bb58620
WhereToPress <stolyarov.mak@gmail.com>
2023-05-27 01:12:03 +0300

    less 19. Router. react-routr-dom -save

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-      <Dialogs />
-      {/* <Profile /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" Component={Dialogs} />
+            <Route path="/profile" Component={Profile} />
+          </Routes>
+        </div>
       </div>
-      
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import style from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={style.nav}>
-      <div className={`${style.item} ${style.active}`}>
-        <a>Profile</a>
+      <div className={`${style.item}`}>
+        <a href="/profile">Profile</a>
       </div>
       <div className={style.item}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={style.item}>
         <a>News</a>
