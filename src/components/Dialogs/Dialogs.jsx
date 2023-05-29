0e59699b41e39fb01b3a1f42487ac2a1c76686e7
WhereToPress <stolyarov.mak@gmail.com>
2023-05-29 14:56:50 +0300

    Upgrade dialog's page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,27 @@ import style from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            {/* Dialogs */}
+            <div className={style.dialogs__items}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Valeria
+                </div>
+                <div className={style.dialog}>
+                    Lida
+                </div>
+                <div className={style.dialog}>
+                    Sergey
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you</div>
+                <div className={style.message}>Good</div>
+
+            </div>
         </div>
+        
     );
 };
 
