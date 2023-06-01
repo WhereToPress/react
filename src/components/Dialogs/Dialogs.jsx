a9acb2415330a07c2dd0782dcb57862871e89cfa
WhereToPress <stolyarov.mak@gmail.com>
2023-06-01 16:31:03 +0300

    less 24. Create object's array: postData in file MyPosts.jsx and dialogsData, messagesData in file Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,20 +18,32 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+    let dialogsData = [
+        {name: "Valeria", id: 1},
+        {name: "Lidia", id: 2},
+        {name: "Sergey", id: 3},
+        {name: "Roman", id: 4},
+        {name: "Valera", id: 5}
+    ]
+    let messagesData = [
+        {id: 1, message: "Hi!"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "Good job!"}
+    ]
     return (
         <div className={style.dialogs}>
             {/* Dialogs */}
             <div className={style.dialogs__items}>
-                <DialigItem name = "Valeria" id = "1" />
-                <DialigItem name = "Lidia" id = "2" />
-                <DialigItem name = "Sergey" id = "3" />
-                <DialigItem name = "Roman" id = "4" />
-                <DialigItem name = "Valera" id = "5" />
+                <DialigItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialigItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialigItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialigItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialigItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div>
             <div className={style.messages}>
-                <Message message = "Hi!" />
-                <Message message = "How are you?" />
-                <Message message = "Good!" />
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
         
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import style from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
 const MyPosts = () => {
+  let postData = [
+    {id: 1, message: "Hello, World!", likesCount: 15},
+    {id: 2, message: "It's my first post", likesCount: 20}
+  ]
   return (
     <div className={style.postsBlock}>
       <h3>My posts</h3>
@@ -14,8 +18,8 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={style.posts}>
-        <Post message="Hello, World!" likesCount="15" />
-        <Post message="It's my first post" likesCount="20" />
+        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
       </div>
     </div>
   );
